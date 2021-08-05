315f83a78e53aadbf538aac55366b3164b3488a2
Dzmitriy Ilyukovich <mr.ilyukovich@gmail.com>
2021-08-05 21:57:19 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable (number) {
-    const arrSingle = [
+    const arrBeforeNineteen = [
         '',
         'one',
         'two',
@@ -10,8 +10,6 @@ module.exports = function toReadable (number) {
         'seven',
         'eight',
         'nine',
-    ],
-        arrDecimal = [
         'ten',
         'eleven',
         'twelve',
@@ -23,56 +21,76 @@ module.exports = function toReadable (number) {
         'eighteen',
         'nineteen'
     ],
-        arrTwoDigit = [
+        arrAfterTwenty = [
         '',
         '',
-        'twenty', 
-        'thirty', 
-        'forty', 
-        'fifty', 
-        'sixty', 
-        'seventy', 
-        'eighty', 
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
         'ninety'
-    ];
+        ];
 
     let result = '';
-    switch(number.length) {
-        case(1):
-        console.log(arrSingle[number]);
-            return (result = arrSingle[number]);
-            
-        case(2):
-            if (number >= 10 && number <= 19) {
-                return (result = arrDecimal[number % 10]);
-            } else {
-                result = `${arrTwoDigit[Math.round(number / 10)]} + ' ' + ${arrSingle[number % 10]}`;
-            }
-            break;
-        case(3):
-            if (number >= 100 && number <= 109 ||
-                number >= 200 && number <= 209 ||
-                number >= 300 && number <= 309 ||
-                number >= 400 && number <= 409 ||
-                number >= 500 && number <= 509 ||
-                number >= 600 && number <= 609 ||
-                number >= 700 && number <= 709 ||
-                number >= 800 && number <= 809 ||
-                number >= 900 && number <= 909) {
-                    return (result = `${arrSingle[Math.round(number / 100)]} + ' hundred ' + ${arrSingle[Math.round(number % 100)]}`);
-            } else if (
-                number >= 110 && number < 120 ||
-                number >= 210 && number < 220 ||
-                number >= 310 && number < 320 ||
-                number >= 410 && number < 420 ||
-                number >= 510 && number < 520 ||
-                number >= 610 && number < 620 ||
-                number >= 710 && number < 720 ||
-                number >= 810 && number < 820 ||
-                number >= 910 && number < 920) {
-                    return (result = `${arrSingle[Math.round(number / 100)]} + ' hundred ' + ${arrDecimal[Math.round(number / 10) % 10]}`);
-            } else {
-                return (result = `${arrSingle[Math.round(number / 100)]} + ' hundred ' + ${arrTwoDigit[Math.round(number / 10) % 10]} + ' ' + ${arrSingle[number % 100]}`);
-            }
+    if (number == 0) {
+        return 'zero';
+    } else if (number > 0 && number <= 19) {
+        return (result = arrBeforeNineteen[number]);
+    } else if (
+        number == 20 || 
+        number == 30 || 
+        number == 40 || 
+        number == 50 || 
+        number == 60 || 
+        number == 70 || 
+        number == 80 || 
+        number == 90) {
+        return (result = `${arrAfterTwenty[number / 10]}`);
+    } else if (number > 20 && number < 100) {
+        return (result = `${arrAfterTwenty[Math.floor(number / 10)]} ${
+            arrBeforeNineteen[number % 10]
+        }`);
+    } else if (
+        number == 100 ||
+        number == 200 ||
+        number == 300 ||
+        number == 400 ||
+        number == 500 ||
+        number == 600 ||
+        number == 700 ||
+        number == 800 ||
+        number == 900
+    ) {
+        return (result = `${arrBeforeNineteen[number / 100]} hundred`);
+    } else if (
+        number % 10 == 0 &&
+        number != 110 &&
+        number != 210 &&
+        number != 310 &&
+        number != 410 &&
+        number != 510 &&
+        number != 610 &&
+        number != 710 &&
+        number != 810 &&
+        number != 910
+        ) {
+        return (result = `${arrBeforeNineteen[Math.floor(number / 100)]} hundred ${arrAfterTwenty[Math.floor(number / 10) % 10]}`);
+    } else if (
+        (number > 100 && number <= 119) ||
+        (number > 200 && number <= 219) ||
+        (number > 300 && number <= 319) ||
+        (number > 400 && number <= 419) ||
+        (number > 500 && number <= 519) ||
+        (number > 600 && number <= 619) ||
+        (number > 700 && number <= 719) ||
+        (number > 800 && number <= 819) ||
+        (number > 900 && number <= 919)
+    ) {
+        return (result = `${arrBeforeNineteen[Math.floor(number / 100)]} hundred ${arrBeforeNineteen[number % 100]}`);
+    } else {
+        return (result = `${arrBeforeNineteen[Math.floor(number / 100)]} hundred ${arrAfterTwenty[Math.floor(number / 10) % 10]} ${arrBeforeNineteen[number % 10]}`);
     }
 };
