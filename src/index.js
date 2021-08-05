235d91d7ee3caf79907dc77adca4fd4184bd006c
Dzmitriy Ilyukovich <mr.ilyukovich@gmail.com>
2021-08-05 10:07:25 +0300

    feat: create arr

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
 module.exports = function toReadable (number) {
  
-    
-}
+    if (number > 0 && number < 1000) {
+        const arrSingle = [
+            '',
+            'one',
+            'two',
+            'three',
+            'four',
+            'five',
+            'six',
+            'seven',
+            'eight',
+            'nine',
+        ],
+            arrDecimal = [
+            'ten',
+            'eleven',
+            'twelve',
+            'thirteen',
+            'fourteen',
+            'fifteen',
+            'sixteen',
+            'seventeen',
+            'eighteen',
+            'nineteen'
+        ],
+            arrTwoDigit = [
+            '',
+            '',
+            'twenty', 
+            'thirty', 
+            'forty', 
+            'fifty', 
+            'sixty', 
+            'seventy', 
+            'eighty', 
+            'ninety'
+        ];
+
+        
+    } return 'write number 0 - 999';
+};
